de728207029c19b4f1265a9e122a5155b273c614
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-10-14 14:43:53 -0500

    menu structure ok

--- main.js
+++ main.js
@@ -1,45 +1,67 @@
-document.getElementById(`one`).addEventListener("mouseover", degreeProgression);
-// document.getElementById(`one`).addEventListener("mouseout", degreeStop);
+// document.getElementById(`one`).addEventListener("mouseover", degreeProgression);
+// // document.getElementById(`one`).addEventListener("mouseout", degreeStop);
 
 
 
 
-function degreeProgression() {
+// function degreeProgression() {
 
-	let counter = 0;
-	let reset = 50;
+// 	let counter = 0;
+// 	let reset = 50;
 
-	const resetRun = setInterval(() => {
-		{
-			if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') == 1) {
+// 	const resetRun = setInterval(() => {
+// 		{
+// 			if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') == 1) {
 
-				if (counter == reset) {
-					counter = 0;
+// 				if (counter == reset) {
+// 					counter = 0;
 
-				}
-				counter++
-				console.log(counter)
-				document.getElementById(`one`).style.backgroundImage = `radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%), 
-									repeating-radial-gradient(
-									transparent ${counter - 35}%, 
-									black ${counter - 45}%, 
-									transparent ${counter - 50}%,
-									black ${counter - 25}%, 
-									transparent ${counter - 10}%),
-									radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)`;
+// 				}
+// 				counter++
+// 				console.log(counter)
+// 				document.getElementById(`one`).style.backgroundImage = `radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%), 
+// 									repeating-radial-gradient(
+// 									transparent ${counter - 35}%, 
+// 									black ${counter - 45}%, 
+// 									transparent ${counter - 50}%,
+// 									black ${counter - 25}%, 
+// 									transparent ${counter - 10}%),
+// 									radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)`;
 
 
-			} else if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') !== 1) {
-				clearInterval(resetRun);
-				degreeStop();
-			}
-		}
+// 			} else if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') !== 1) {
+// 				clearInterval(resetRun);
+// 				degreeStop();
+// 			}
+// 		}
 
-	}, 50);
-}
+// 	}, 50);
+// }
 
-function degreeStop() {
-	console.log("done");
-	document.getElementById(`one`).style.backgroundImage = 'radial-gradient(rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.75) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)';
+// function degreeStop() {
+// 	console.log("done");
+// 	document.getElementById(`one`).style.backgroundImage = 'radial-gradient(rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.75) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)';
 
-}
+// }
+
+function barChoice() {
+    if (document.getElementById('back0').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack0.png';
+    }
+    if (document.getElementById('back1').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack1.png';
+    }
+    if (document.getElementById('back2').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack2.png';
+    }
+    if (document.getElementById('back3').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack3.png';
+    }
+    if (document.getElementById('back4').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack4.png';
+    }
+    if (document.getElementById('back5').checked) {
+        document.getElementById('chosenOne').src = './asset/cardBack5.png';
+    }
+
+} deckChoice();
